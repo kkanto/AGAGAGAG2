a1c333d55a0cb5bf367130feb5cf81e596cdc26d
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-12 22:26:23 +0200

    Create maven project

---
+++ src/main/java/de/allianz/AGAGAGAG2/Agagagag2Application.java
@@ -0,0 +1,13 @@
+package de.allianz.AGAGAGAG2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agagagag2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agagagag2Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/AGAGAGAG2/Agagagag2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.AGAGAGAG2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agagagag2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
